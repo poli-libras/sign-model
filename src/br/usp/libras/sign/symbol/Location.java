20bda62892dcfd77a9427cd44e05f4f4aa0dbeb3
fabio <fabio.exe@gmail.com>
2013-09-24 12:44:18 -0300

    Novas enumerações adicionadas

--- src/br/usp/libras/sign/symbol/Location.java
+++ src/br/usp/libras/sign/symbol/Location.java
@@ -13,16 +13,19 @@ package br.usp.libras.sign.symbol;
  */
 public enum Location {
     
-        ESPACO_NEUTRO(LocationGroup.ESPACO_NEUTRO),
+    ESPACO_NEUTRO(LocationGroup.ESPACO_NEUTRO), ESPACO_NEUTRO_DIREITO(LocationGroup.ESPACO_NEUTRO), ESPACO_NEUTRO_ESQUERDO(LocationGroup.ESPACO_NEUTRO),
 	
-	TOPO_DA_CABECA(LocationGroup.CABECA), TESTA(LocationGroup.CABECA), ROSTO(LocationGroup.CABECA),
+    TESTA(LocationGroup.CABECA), TESTA_DIREITA(LocationGroup.CABECA), TESTA_ESQUERDA(LocationGroup.CABECA),
 	ROSTO_SUPERIOR(LocationGroup.CABECA), ROSTO_INFERIOR(LocationGroup.CABECA), ORELHA(LocationGroup.CABECA),
 	OLHOS(LocationGroup.CABECA), NARIZ(LocationGroup.CABECA), BOCA(LocationGroup.CABECA), 
-	BOCHECHAS(LocationGroup.CABECA), QUEIXO(LocationGroup.CABECA),
+	BOCHECHAS(LocationGroup.CABECA), TOPO_DA_CABECA(LocationGroup.CABECA), ROSTO(LocationGroup.CABECA),
+	QUEIXO(LocationGroup.CABECA), QUEIXO_DIREITO(LocationGroup.CABECA), QUEIXO_ESQUERDO(LocationGroup.CABECA),
 	
-	PESCOCO(LocationGroup.TRONCO), OMBRO(LocationGroup.TRONCO), BUSTO(LocationGroup.TRONCO),
-	ESTOMAGO(LocationGroup.TRONCO), CINTURA(LocationGroup.TRONCO), BRACOS(LocationGroup.TRONCO),
-	ANTEBRACO(LocationGroup.TRONCO), COTOVELO(LocationGroup.TRONCO), PULSO(LocationGroup.TRONCO),
+	PESCOCO(LocationGroup.TRONCO), OMBRO(LocationGroup.TRONCO), OMBRO_DIREITO(LocationGroup.TRONCO),
+	OMBRO_ESQUERDO(LocationGroup.TRONCO), BUSTO(LocationGroup.TRONCO), BUSTO_DIREITO(LocationGroup.TRONCO),
+	BUSTO_ESQUERDO(LocationGroup.TRONCO), ESTOMAGO(LocationGroup.TRONCO), CINTURA(LocationGroup.TRONCO), 
+	BRACOS(LocationGroup.TRONCO), ANTEBRACO(LocationGroup.TRONCO), COTOVELO(LocationGroup.TRONCO), 
+	PULSO(LocationGroup.TRONCO), QUADRIL(LocationGroup.TRONCO),
 	
 	PALMA(LocationGroup.MAO), COSTAS_DA_MAO(LocationGroup.MAO), LADO_INDICADOR(LocationGroup.MAO),
 	LADO_DEDO_MINIMO(LocationGroup.MAO), DEDOS(LocationGroup.MAO), PONTA_DOS_DEDOS(LocationGroup.MAO),
