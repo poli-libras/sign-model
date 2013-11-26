d36d3692dc97f57ae120ead9c788e336ce5a9a09
fabio <fabio.exe@gmail.com>
2013-11-26 14:32:56 -0200

    Location - getString e getFilename adicionados

--- src/br/usp/libras/sign/symbol/Location.java
+++ src/br/usp/libras/sign/symbol/Location.java
@@ -13,35 +13,45 @@ package br.usp.libras.sign.symbol;
  */
 public enum Location {
     
-    ESPACO_NEUTRO(LocationGroup.ESPACO_NEUTRO), ESPACO_NEUTRO_DIREITO(LocationGroup.ESPACO_NEUTRO), ESPACO_NEUTRO_ESQUERDO(LocationGroup.ESPACO_NEUTRO),
+    ESPACO_NEUTRO(LocationGroup.ESPACO_NEUTRO, "Espaço Neutro", "espaco_neutro"), ESPACO_NEUTRO_DIREITO(LocationGroup.ESPACO_NEUTRO, "Espaço Neutro Direito", "espaco_neutro_direito"), ESPACO_NEUTRO_ESQUERDO(LocationGroup.ESPACO_NEUTRO, "Espaço Neutro Esquerdo", "espaco_neutro_esquerdo"),
 	
-    TESTA(LocationGroup.CABECA), TESTA_DIREITA(LocationGroup.CABECA), TESTA_ESQUERDA(LocationGroup.CABECA),
-	ROSTO_SUPERIOR(LocationGroup.CABECA), ROSTO_INFERIOR(LocationGroup.CABECA), ORELHA(LocationGroup.CABECA),
-	OLHOS(LocationGroup.CABECA), NARIZ(LocationGroup.CABECA), BOCA(LocationGroup.CABECA), 
-	BOCHECHAS(LocationGroup.CABECA), TOPO_DA_CABECA(LocationGroup.CABECA), ROSTO(LocationGroup.CABECA),
-	QUEIXO(LocationGroup.CABECA), QUEIXO_DIREITO(LocationGroup.CABECA), QUEIXO_ESQUERDO(LocationGroup.CABECA),
+    TESTA(LocationGroup.CABECA, "Centro da testa", "testa"), TESTA_DIREITA(LocationGroup.CABECA, "Parte direita da testa", "testa_direita"), TESTA_ESQUERDA(LocationGroup.CABECA, "Parte esquerda da testa", "testa_esquerda"),
+	ROSTO_SUPERIOR(LocationGroup.CABECA, "Parte superior do rosto", "rosto_superior"), ROSTO_INFERIOR(LocationGroup.CABECA, "Parte inferior do rosto", "rosto_inferior"), ORELHA(LocationGroup.CABECA, "Orelha", "orelha"),
+	OLHOS(LocationGroup.CABECA, "Olhos", "olhos"), NARIZ(LocationGroup.CABECA, "Nariz", "nariz"), BOCA(LocationGroup.CABECA, "Boca", "boca"), 
+	BOCHECHAS(LocationGroup.CABECA, "Bochechas", "bochechas"), TOPO_DA_CABECA(LocationGroup.CABECA, "Topo da Cabeça", "topo_da_cabeca"), ROSTO(LocationGroup.CABECA, "Centro do rosto", "rosto"),
+	QUEIXO(LocationGroup.CABECA, "Centro do queixo", "queixo"), QUEIXO_DIREITO(LocationGroup.CABECA, "Parte direita do queixo", "queixo_direito"), QUEIXO_ESQUERDO(LocationGroup.CABECA, "Parte esquerda do queixo", "queixo_esquerdo"),
 	
-	PESCOCO(LocationGroup.TRONCO), OMBRO(LocationGroup.TRONCO), OMBRO_DIREITO(LocationGroup.TRONCO),
-	OMBRO_ESQUERDO(LocationGroup.TRONCO), BUSTO(LocationGroup.TRONCO), BUSTO_DIREITO(LocationGroup.TRONCO),
-	BUSTO_ESQUERDO(LocationGroup.TRONCO), ESTOMAGO(LocationGroup.TRONCO), CINTURA(LocationGroup.TRONCO), 
-	BRACOS(LocationGroup.TRONCO), ANTEBRACO(LocationGroup.TRONCO), COTOVELO(LocationGroup.TRONCO), 
-	PULSO(LocationGroup.TRONCO), QUADRIL(LocationGroup.TRONCO),
+	PESCOCO(LocationGroup.TRONCO, "Pescoço", "pescoco"), OMBRO(LocationGroup.TRONCO, "Centro do ombro", "ombro"), OMBRO_DIREITO(LocationGroup.TRONCO, "Parte direita do ombro", "ombro_direito"),
+	OMBRO_ESQUERDO(LocationGroup.TRONCO, "Parte esquerda do ombro", "ombro_esquerdo"), BUSTO(LocationGroup.TRONCO, "Busto", "busto"), BUSTO_DIREITO(LocationGroup.TRONCO, "Parte direita do busto", "busto_direito"),
+	BUSTO_ESQUERDO(LocationGroup.TRONCO, "Parte esquerda do busto", "busto_esquerdo"), ESTOMAGO(LocationGroup.TRONCO, "Estômago", "estomago"), CINTURA(LocationGroup.TRONCO, "Cintura", "cintura"), 
+	BRACO(LocationGroup.TRONCO, "Braço", "braco"), ANTEBRACO(LocationGroup.TRONCO, "Antebraço", "antebraco"), COTOVELO(LocationGroup.TRONCO, "Cotovelo", "cotovelo"), 
+	PULSO(LocationGroup.TRONCO, "Pulso", "pulso"), QUADRIL(LocationGroup.TRONCO, "Quadril", "quadril"),
 	
-	PALMA(LocationGroup.MAO), COSTAS_DA_MAO(LocationGroup.MAO), LADO_INDICADOR(LocationGroup.MAO),
-	LADO_DEDO_MINIMO(LocationGroup.MAO), DEDOS(LocationGroup.MAO), PONTA_DOS_DEDOS(LocationGroup.MAO),
-	DEDO_MINIMO(LocationGroup.MAO), ANULAR(LocationGroup.MAO), DEDO_MEDIO(LocationGroup.MAO), 
-	INDICADOR(LocationGroup.MAO), POLEGAR(LocationGroup.MAO);
+	PALMA(LocationGroup.MAO, "Palma da mão", "palma"), COSTAS_DA_MAO(LocationGroup.MAO, "Costas da mão", "costas_da_mao"), LADO_DEDO_INDICADOR(LocationGroup.MAO, "Lateral da mão (lado dedo indicador)", "lado_dedo_indicador"),
+	LADO_DEDO_MINIMO(LocationGroup.MAO, "Lateral da mão (lado dedo mínimo)", "lado_dedo_minimo"), DEDOS(LocationGroup.MAO, "Região dos dedos da mão", "dedos"), PONTA_DOS_DEDOS(LocationGroup.MAO, "Ponta dos dedos da mão", "ponta_dos_dedos"),
+	DEDO_MINIMO(LocationGroup.MAO, "Dedo mínimo", "dedo_minimo"), ANULAR(LocationGroup.MAO, "Dedo anular", "dedo_anular"), DEDO_MEDIO(LocationGroup.MAO, "Dedo médio", "dedo_medio"), 
+	INDICADOR(LocationGroup.MAO, "Dedo indicador", "dedo_indicador"), POLEGAR(LocationGroup.MAO, "Dedo polegar", "dedo_polegar");
 		
 	private final LocationGroup group;
+	private String string;
+	private String filename;
 	
-	private Location(LocationGroup group) {
+	private Location(LocationGroup group, String string, String filename) {
 		
 		this.group = group;
+		this.string = string;
+		this.filename = filename;
 	}
 	
 	public LocationGroup locationGroup() {
 		return this.group;
 	}
 	
-
+	public String getString() {
+		return this.string;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
 }
