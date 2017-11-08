765d7fb916d4da08ba14d3283c6ded1e49afa7a6
leonardofl <leonardofl87@gmail.com>
2017-11-08 18:59:08 -0200

    comentários Path

--- src/br/usp/libras/sign/transition/Path.java
+++ src/br/usp/libras/sign/transition/Path.java
@@ -1,9 +1,20 @@
 package br.usp.libras.sign.transition;
 
 // TODO documentar com figura
+/**
+ * Sentido (horário vs anti-horário) é do ponto de vista do emissor.
+ * XY é o plano da tela.
+ * Oz é perpendicular à tela.
+ */
 public enum Path {
 
-    LINEAR, CIRCULAR_HORARIO_EM_XY, CIRCULAR_ANTI_HORARIO_EM_XY, CIRCULAR_HORARIO_EM_XZ, CIRCULAR_ANTI_HORARIO_EM_XZ, CIRCULAR_HORARIO_EM_YZ, CIRCULAR_ANTI_HORARIO_EM_YZ;
+    LINEAR, 
+    CIRCULAR_HORARIO_EM_XY, 
+    CIRCULAR_ANTI_HORARIO_EM_XY, 
+    CIRCULAR_HORARIO_EM_XZ, 
+    CIRCULAR_ANTI_HORARIO_EM_XZ, 
+    CIRCULAR_HORARIO_EM_YZ, 
+    CIRCULAR_ANTI_HORARIO_EM_YZ;
 
     public boolean horario() {
         return this == CIRCULAR_HORARIO_EM_XY || this == Path.CIRCULAR_HORARIO_EM_XZ
